be8edd7dfdf0ed6f085ce37472904dd9de2261c0
Adam Staros <staros.adam@gmail.com>
2025-12-28 20:42:55 +0100

    Moved return type validation to adapter specific implementation

--- macros/databricks/fake_salary.sql
+++ macros/databricks/fake_salary.sql
@@ -1,4 +1,8 @@
 {% macro databricks__fake_salary(seed_column, min_salary=none, max_salary=none, return_type='decimal(10,2)') %}
+    {% set supported_return_types = ['decimal(10,2)', 'double', 'float', 'string'] %}
+    {% if return_type not in supported_return_types %}
+        {% do exceptions.raise_compiler_error("Invalid return_type for Databricks: '" ~ return_type ~ "'. Supported return types are: " ~ (supported_return_types | join(", "))) %}
+    {% endif %}
     {% if min_salary is none %} {% set min_salary = 2000 %} {% endif %}
     {% if max_salary is none %} {% set max_salary = 80000 %} {% endif %}
     {% set salary_range = max_salary - min_salary %}
--- macros/fake_age.sql
+++ macros/fake_age.sql
@@ -1,8 +1,4 @@
 {% macro fake_age(seed_column, minimum_age=0, maximum_age=120, return_type='int') %}
-    {% set supported_return_types = ['string', 'int'] %}
-    {% if return_type not in supported_return_types %}
-        {% do exceptions.raise_compiler_error("Invalid return_type: '" ~ return_type ~ "'. Supported return types are: " ~ (supported_return_types | join(", "))) %}
-    {% endif %}
     {% if minimum_age < 0 %}
         {% do exceptions.raise_compiler_error("minimum_age must be >= 0, but got: " ~ minimum_age) %}
     {% endif %}
